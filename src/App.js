83e3a766468fcab09e635471eccc80e81f144ac0
KazhymukhanYerkinov <61079138+KazhymukhanYerkinov@users.noreply.github.com>
2021-03-08 22:14:55 +0600

    youtube-18

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Profile from './components/Profile/Profile';
 import Sidebar from './components/Sidebar/Sidebar';
+import Dialogs from './components/Dialogs/Dialogs';
 
 import './App.css';
+
 
 function App() {
   return (
     <div className = 'app-wrapper'>
       <Header />
-
       <Sidebar />
 
-      <Profile />
+      <div className = 'app-wrapper-content'>
+        {/* <Profile /> */}
+        <Dialogs />
+      </div>
+      
     </div>
   );
 }
---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import cls from './Dialogs.module.css';
+
+const Dialogs = () => {
+    return (
+        <div>
+            Dialogs
+        </div>
+    )
+}
+
+export default Dialogs;
