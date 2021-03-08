79a2b0aad17065c582b09e8d3bd418ffb5f02538
KazhymukhanYerkinov <61079138+KazhymukhanYerkinov@users.noreply.github.com>
2021-03-08 22:33:14 +0600

    youtube-19

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route } from 'react-router';
 
 import Header from './components/Header/Header';
 import Profile from './components/Profile/Profile';
@@ -8,17 +9,16 @@ import Dialogs from './components/Dialogs/Dialogs';
 import './App.css';
 
 
+
 function App() {
   return (
     <div className = 'app-wrapper'>
       <Header />
       <Sidebar />
-
       <div className = 'app-wrapper-content'>
-        {/* <Profile /> */}
-        <Dialogs />
+        <Route path = '/dialogs' component = { Profile } />
+        <Route path = '/profile' component = { Dialogs } />
       </div>
-      
     </div>
   );
 }
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import cls from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
