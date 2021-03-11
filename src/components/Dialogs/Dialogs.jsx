edbe2be89045638c20e09d59a2300ebee0eee20b
KazhymukhanYerkinov <61079138+KazhymukhanYerkinov@users.noreply.github.com>
2021-03-11 17:31:36 +0600

    youtube-21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
 
+import cls from './Dialogs.module.css';
+
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className = {cls.dialogs}>
+            <div className = {cls.dialogsItems}>
+                <div className = {cls.dialog}>
+                    Andrey
+                </div>
+                <div className = {cls.dialog}>
+                    Dimych
+                </div>
+                <div className = {cls.dialog}>
+                    Sveta
+                </div>
+                <div className = {cls.dialog}>
+                    Sasha
+                </div>
+                <div className = {cls.dialog}>
+                    Valera
+                </div>
+            </div>
+
+            <div className = {cls.messages}>
+                <div className = {cls.message}>
+                    Hi
+                </div>
+                <div className = {cls.message}>
+                    How is your it-kamasutra
+                </div>
+                <div className = {cls.message}>
+                    Yo
+                </div>
+            </div>
         </div>
     )
 }
