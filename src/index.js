46817226d72e6b813cda465a6f5a860027d42649
KazhymukhanYerkinov <61079138+KazhymukhanYerkinov@users.noreply.github.com>
2021-03-13 20:26:26 +0600

    youtube-37

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
-import state from './redux/state';
+import store from './redux/state';
 import reportWebVitals from './reportWebVitals';
-import { addPost, updateNewPostText, subscribe } from './redux/state'
+
 
 import './index.css';
 
@@ -16,15 +16,15 @@ export let rerenderEntireTree = (state) => {
   ReactDOM.render(
     <BrowserRouter>
       <App 
-        state={state} 
-        addPost={addPost}
-        updateNewPostText = { updateNewPostText }
+        state={ state } 
+        addPost={store.addPost.bind(store)}
+        updateNewPostText = { store.updateNewPostText.bind(store) }
       />
     </BrowserRouter>, document.getElementById('root')
   );
 }
-rerenderEntireTree(state);
-subscribe(rerenderEntireTree)
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree)
 
 
 // If you want to start measuring performance in your app, pass a function
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,58 +1,66 @@
-let rerenderEntireTree = () => {
-    console.log('State is changed');
-}
-
-let state = {
+let store = {
+  _state: {
     profilePage: {
-        posts: [
-            { id: 1, message: 'Hi, how are you?', likeCount: 12 },
-            { id: 2, message: 'It\'s my first post', likeCount: 8 },
-        ],
-        newPostText: 'it-kamasutra.com',
+      posts: [
+        { id: 1, message: 'Hi, how are you?', likeCount: 12 },
+        { id: 2, message: 'It\'s my first post', likeCount: 8 },
+      ],
+      newPostText: 'it-kamasutra.com',
     },
     dialogsPage: {
-        dialogs: [
-            { id: 1, name: 'Andrey' },
-            { id: 2, name: 'Dimych' },
-            { id: 3, name: 'Sveta' },
-            { id: 4, name: 'Sasha' },
-            { id: 5, name: 'Valera' },
-            { id: 6, name: 'Viktor' },
-        ],
-        messages: [
-            { id: 1, message: 'Hi' },
-            { id: 2, message: 'How is your it-kamasutra' },
-            { id: 3, message: 'Yo' },
-            { id: 4, message: 'Yo' },
-            { id: 5, message: 'Yo' },
-            { id: 6, message: 'Yaho' },
-        ],
+      dialogs: [
+        { id: 1, name: 'Andrey' },
+        { id: 2, name: 'Dimych' },
+        { id: 3, name: 'Sveta' },
+        { id: 4, name: 'Sasha' },
+        { id: 5, name: 'Valera' },
+        { id: 6, name: 'Viktor' },
+      ],
+      messages: [
+        { id: 1, message: 'Hi' },
+        { id: 2, message: 'How is your it-kamasutra' },
+        { id: 3, message: 'Yo' },
+        { id: 4, message: 'Yo' },
+        { id: 5, message: 'Yo' },
+        { id: 6, message: 'Yaho' },
+      ],
     }
-}
+  },
 
-// Жаңа пост қосатын функция
-export const addPost = () => {
+  // state қайтаратын функция
+  getState() {
+    return this._state;
+  },
+
+  // State өзгерген кезде UI салатын функция
+  _callSubscriber() {
+    console.log('State changed')
+  },
+
+  // Жаңа пост қосатын функция
+  addPost() {
 
     // Жаңадан пост объект жасау
     let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likeCount: 0
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likeCount: 0
     };
-    state.profilePage.posts.push(newPost)
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
+    this._state.profilePage.posts.push(newPost)
+    this._state.profilePage.newPostText = '';
+    this._callSubscriber(this._state);
+  },
+
+  // Textarea да жазылған әрбір өзгерістерді салып отыру
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+
+  // index.js тағы рендер функцияны шақыру
+  subscribe(observer) {
+    this._callSubscriber = observer;
+  },
 }
 
-// Textarea да жазылған әрбір өзгерістерді салып отыру
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
-
-// index.js тағы рендер функцияны шақыру
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer;
-}
-
-export default state;
+export default store;
