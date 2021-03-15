53b7fc45349ef3834dc12d79a5b6f5c202d0e7b1
KazhymukhanYerkinov <61079138+KazhymukhanYerkinov@users.noreply.github.com>
2021-03-15 23:37:36 +0600

    youtube-54

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,9 +6,7 @@ import cls from './Users.module.css';
 
 
 class Users extends React.Component {
-  constructor(props) {
-    super(props);
-    
+  componentDidMount() {
     axios.get(`https://social-network.samuraijs.com/api/1.0/users`).then(response => {
         this.props.setUsers(response.data.items)
     })
